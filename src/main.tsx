f6421ec3c9637fa4acaca85a65eeac11a9636ad8
Alexis Reyes <alexis_mreyes@hotmail.com>
2024-08-17 15:28:51 -0600

    Fixing some bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home.tsx";
