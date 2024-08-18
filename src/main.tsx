749378a114a6729293066b88886a8ebe3e7e68f4
alexismreyes <alexis_mreyes@hotmail.com>
2024-08-18 11:37:22 -0600

    Added basename in main.tsx again but adding base to vite.config.js too

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,12 @@ import Sortable from './components/Sortable.tsx';
 import Dev from './components/Dev.tsx';
 import Page404 from './components/Page404.tsx';
 
+const basename =
+  process.env.NODE_ENV === 'production' ? '/sortable-table-review/' : '/';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
