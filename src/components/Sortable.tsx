1f0eee3923f60c46707419c81b7980598d0c369c
alexismreyes <alexis_mreyes@hotmail.com>
2024-08-20 14:49:45 -0600

    Implemented useMemo for Sortable component

--- src/components/Sortable.tsx
+++ src/components/Sortable.tsx
@@ -1,5 +1,5 @@
 import '../solution/Solution.ts';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { Person } from '../solution/People.ts';
 import { peopleArray, personKeys, sortByProp } from '../solution/Solution.ts';
 import PeopleTable from './PeopleTable.tsx';
@@ -7,23 +7,29 @@ import '../assets/Sortable.css';
 
 const Sortable = () => {
   const [people, setPeople] = useState<Person[]>([]);
-  const [sortedPeople, setSortedPeople] = useState<Person[]>([]);
+  /* const [sortedPeople, setSortedPeople] = useState<Person[]>([]); */
   const [selectedKey, setSelectedKey] = useState<keyof Person>('Name');
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
 
   useEffect(() => {
     setPeople(peopleArray);
-    sortPeople(selectedKey, sortOrder);
+    /* sortPeople(selectedKey, sortOrder); */
   }, []);
 
-  useEffect(() => {
+  /*  useEffect(() => {
     sortPeople(selectedKey, sortOrder);
-  }, [selectedKey, sortOrder, people]);
+  }, [selectedKey, sortOrder, people]); */
 
-  const sortPeople = (key: keyof Person, order: 'asc' | 'desc') => {
+  //Memoize the sortedPeople array to avoid unnecessary recalculations
+  const sortedPeople = useMemo(() => {
+    const sorted = sortByProp(people, selectedKey);
+    return sortOrder === 'asc' ? sorted : sorted.reverse();
+  }, [people, selectedKey, sortOrder]);
+
+  /* const sortPeople = (key: keyof Person, order: 'asc' | 'desc') => {
     const sorted = sortByProp(people, key);
     setSortedPeople(order === 'asc' ? sorted : sorted.reverse());
-  };
+  }; */
 
   const handleSort = (key: keyof Person) => {
     const order = key === selectedKey && sortOrder === 'asc' ? 'desc' : 'asc';
